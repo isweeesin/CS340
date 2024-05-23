8cabd48dae38a026a414f4ce45ef4484816b2c32
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2024-05-23 17:23:35 -0500

    Update update_customer.js
    added comments

--- public/js/update_customer.js
+++ public/js/update_customer.js
@@ -1,23 +1,33 @@
+
+// Citation for the following function:  update_customer.js
+// Date: 5/22/2024
+// Based on:
+// Starter code for 'Developing in Node.JS'.
+// Source URL: https://canvas.oregonstate.edu/courses/1958399/pages/exploration-developing-in-node-dot-js?module_item_id=24181856
+
+/* form submission -> execute and get vars based on inputs */
 document.getElementById('update-customer-form-ajax').addEventListener('submit', function(event) {
     event.preventDefault();
 
     let customerId = document.getElementById('mySelect').value;
     let newName = document.getElementById('input-new-name').value;
-    let phoneNumber = document.getElementById('input-phone_number').value;
-    let email = document.getElementById('input-email').value;
+    let phoneNumber = document.getElementById('update-phone_number').value;
+    let email = document.getElementById('update-email').value;
 
+    /* data object, stores values to be used in ajax request */
     let data = {
         customer_id: customerId,
         name: newName,
         phone_number: phoneNumber,
         email: email
     };
-
+    /* async. request, */
     $.ajax({
         url: '/update-customer-ajax',
         type: 'PUT',
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8",
+        /* sucess/error messages */
         success: function(result) {
             alert('Customer updated successfully');
             location.reload();
