7a6dc6d7f3a03ca6bff1b8b431c674944a604cf3
brentwooley95 <138704843+brentwooley95@users.noreply.github.com>
2024-05-23 17:27:42 -0500

    Update delete_customer.js
    added comments

--- public/js/delete_customer.js
+++ public/js/delete_customer.js
@@ -1,9 +1,17 @@
+// Citation for the following function:  delete_customer, deleteRow using ajax
+// Date: 5/22/2024
+// Adapted from:
+// Starter code for 'Developing in Node.JS'.
+// Source URL: https://canvas.oregonstate.edu/courses/1958399/pages/exploration-developing-in-node-dot-js?module_item_id=24181856
+
+
+/* send delete request using ajax link and input data(id) */
 function deleteCustomer(customer_id) {
     let link = '/delete-customer-ajax/';
     let data = {
         id: customer_id
     };
-
+    /* async delete */
     $.ajax({
         url: link,
         type: 'DELETE',
@@ -17,7 +25,7 @@ function deleteCustomer(customer_id) {
         }
     });
 }
-
+/* remove corresponding row */
 function deleteRow(customer_id) {
     let table = document.getElementById("customers-table");
     for (let i = 0, row; row = table.rows[i]; i++) {
